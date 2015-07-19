1b94d532a388addd8ebe741c427c8f1206eb853a
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2015-07-20 00:29:09 +0900

    copy closure library into lib to use it for browser
    I'm not sure it's really the best way, but it's good that I don't have to introduce browserify or webpack only for closure library

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,11 +11,31 @@ module.exports = function (grunt) {
         }
       }
     },
+    copy: {
+      closureLibrary: {
+        files: [
+          {
+            expand: true,
+            cwd: 'node_modules/',
+            src: [
+              'google-closure-library/closure/goog/**/*.js',
+              'google-closure-library/third_party/closure/goog/**/*.js',
+            ],
+            dest: 'app/lib/'
+          }
+        ]
+      }
+    },
     shell: {
       test: { command: 'mocha --require intelli-espower-loader' },
       lint: { command: 'eslint Gruntfile.js app/js/ test' }
     }
   });
+
+  grunt.registerTask('install', [
+    'bower:install',
+    'copy:closureLibrary'
+  ]);
 
   grunt.registerTask('default', [
     'shell:lint',

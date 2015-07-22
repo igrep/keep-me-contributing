a11821a3ed90ec73c619cfbc4d769acd774ddac6
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2015-07-23 00:30:27 +0900

    add second failing test case

--- test/js/ContributionStatusTest.js
+++ test/js/ContributionStatusTest.js
@@ -8,15 +8,30 @@ import ContributionStatus from '../../app/js';
 describe('ContributionStatus', () => {
   describe('#recentlyContributedAt', () => {
     context('given a response of https://api.github.com/users/igrep/events', () => {
-      let contributionStatus = new ContributionStatus(
-        JSON.parse(fs.readFileSync('test/fixtures/api.github.com/events/igrep.json'))
-      );
+      let fromJsonFile = (path) => {
+        return new ContributionStatus(JSON.parse(fs.readFileSync(path)));
+      };
 
-      it('has recentlyContributedAt as recorded in JSON', () => {
-        assert(
-          contributionStatus.recentlyContributedAt.valueOf() === new Date('2015-07-19T16:08:53Z').valueOf()
-        );
+      context('the latest contribution event is a PushEvent', () => {
+        let contributionStatus = fromJsonFile('test/fixtures/api.github.com/events/PushEvent.json');
+
+        it('has recentlyContributedAt same as the latest PushEvent', () => {
+          assert(
+            contributionStatus.recentlyContributedAt.valueOf() === new Date('2015-07-19T16:08:53Z').valueOf()
+          );
+        });
       });
+
+      context('the latest contribution event is a CreateEvent of a repository', () => {
+        let contributionStatus = fromJsonFile('test/fixtures/api.github.com/events/CreateEvent.json');
+
+        it('has recentlyContributedAt same as the latest CreateEvent of a repository', () => {
+          assert(
+            contributionStatus.recentlyContributedAt.valueOf() === new Date('2015-07-18T04:36:51Z').valueOf()
+          );
+        });
+      });
+
     });
   });
 });

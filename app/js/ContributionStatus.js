a6d7c85dc524f78f8b0155da7613b4b5b1698237
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2015-07-24 00:22:01 +0900

    import google closure library so that mocha can refer
    ES6 import didn't work because of this error:
    
    _googleClosureLibraryClosureGoogBootstrapNodejs2['default'].require('goog.arra
                                                                ^
    TypeError: undefined is not a function

--- app/js/ContributionStatus.js
+++ app/js/ContributionStatus.js
@@ -1,3 +1,11 @@
+/*global goog:false*/
+
+if (typeof module !== 'undefined' && module.exports) {
+  require('google-closure-library/closure/goog/bootstrap/nodejs');
+}
+
+goog.require('goog.array');
+
 class ContributionStatus {
   constructor(eventsApiResponse){
     this._recentlyContributedAt = new Date('2015-07-19T16:08:53Z');

d6eab54b4a18c39d562392615f8af0689d944c85
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2015-08-02 21:30:34 +0900

    (WIP) prepare build job for testing
    but seems that I used closure compiler in a wrong way
    
    It just output empty js file

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,8 +39,40 @@ module.exports = function (grunt) {
       }
     },
     shell: {
-      test: { command: 'mocha --require espower-babel/guess test/js/KeepMeContributing/' },
-      lint: { command: 'eslint Gruntfile.js app/js/ test' }
+      buildTest: {
+        command: (
+          [
+            'java -jar node_modules/google-closure-compiler/compiler.jar',
+            '--logging_level=INFO',
+
+            '--process_common_js_modules',
+            '--common_js_entry_module=app/js/KeepMeContributing/index.js',
+
+            '--manage_closure_dependencies',
+            '--new_type_inf',
+
+            '--create_source_map=test/js/sourceMap.json',
+
+            '--allow_es6_out',
+            '--language_in=ECMASCRIPT6',
+            '--language_out=ECMASCRIPT5_STRICT',
+
+            '--jscomp_error=checkTypes',
+            '--jscomp_error=constantProperty',
+            '--jscomp_error=const',
+            '--jscomp_error=visibility',
+            '--jscomp_error=checkRegExp',
+            '--jscomp_error=invalidCasts',
+            '--warning_level=VERBOSE',
+            '--jscomp_warning=checkDebuggerStatement',
+
+            '--js=app/js/**.js',
+            '--js_output_file=test/js/app.js'
+          ].join(' ')
+        )
+      },
+      testServer: { command: 'js-dev-server -S test/ -W test/ --port 9876' },
+      lint: { command: 'eslint Gruntfile.js app/js/ test/js/' }
     }
   });
 
@@ -48,9 +80,14 @@ module.exports = function (grunt) {
     'bower:install',
     'copy:closureLibrary'
   ]);
+  grunt.registerTask('test', [
+    'copy:bowerForTest',
+    'shell:buildTest',
+    'shell:testServer'
+  ]);
 
   grunt.registerTask('default', [
     'shell:lint',
-    'shell:test'
+    'test'
   ]);
 };

aa18994d73fba491c696f8bcfe32619b58c526ac
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2015-08-08 23:42:26 +0900

    use expect.js instead of power-assert

--- app/test/js/KeepMeContributing/ContributionStatusTest.js
+++ app/test/js/KeepMeContributing/ContributionStatusTest.js
@@ -18,31 +18,35 @@ describe('ContributionStatus', function(){
       this.subjectFunction = function(date){
         return this.describedInstance.queryHasContributedAt(date);
       };
+
+      this.expectTheResultToBe = function(date, result) {
+        return this.subjectFunction(date).then(function(result){ expect(result).to.be(false); });
+      };
     });
 
     context('given a response of https://github.com/users/igrep/contributions', function(){
 
       context("the argument date's contribution count is 0", function(){
         it('returns false', function(){
-          return this.subjectFunction(new Date('2015-07-31')).then(function(result){ assert(result === false); });
+          return this.expectTheResultToBe(new Date('2015-07-31'), false);
         });
       });
 
       context("the argument date's contribution count is 2", function(){
         it('returns true', function(){
-          return this.subjectFunction(new Date('2015-07-23')).then(function(result){ assert(result === true); });
+          return this.expectTheResultToBe(new Date('2015-07-23'), true);
         });
       });
 
       context("the argument date's contribution count is 1", function(){
         it('returns true', function(){
-          return this.subjectFunction(new Date('2015-07-29')).then(function(result){ assert(result === true); });
+          return this.expectTheResultToBe(new Date('2015-07-29'), true);
         });
       });
 
       context("the argument date's contribution count is present", function(){
         it('returns false', function(){
-          return this.subjectFunction(new Date('2015-08-01')).then(function(result){ assert(result === false); });
+          return this.expectTheResultToBe(new Date('2015-08-01'), false);
         });
       });
 

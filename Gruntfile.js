bff3f1061cded98fa27e7cb9452d99df4c1a9003
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2015-08-15 18:43:19 +0900

    Cosmetic: delete verbose parentheses

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,35 +27,33 @@ module.exports = function (grunt) {
     },
     shell: {
       buildTest: {
-        command: (
-          [
-            'java -jar node_modules/google-closure-compiler/compiler.jar',
+        command: [
+          'java -jar node_modules/google-closure-compiler/compiler.jar',
 
-            '--only_closure_dependencies',
-            '--closure_entry_point=KeepMeContributing',
+          '--only_closure_dependencies',
+          '--closure_entry_point=KeepMeContributing',
 
-            '--create_source_map=app/test/js/output.js.map',
-            '--source_map_location_mapping=app\\|',
-            '--output_wrapper "%output%\n//# sourceMappingURL=/test/js/output.js.map"',
+          '--create_source_map=app/test/js/output.js.map',
+          '--source_map_location_mapping=app\\|',
+          '--output_wrapper "%output%\n//# sourceMappingURL=/test/js/output.js.map"',
 
-            '--allow_es6_out',
-            '--language_in=ECMASCRIPT6',
-            '--language_out=ECMASCRIPT5_STRICT',
+          '--allow_es6_out',
+          '--language_in=ECMASCRIPT6',
+          '--language_out=ECMASCRIPT5_STRICT',
 
-            '--jscomp_error=constantProperty',
-            '--jscomp_error=const',
-            '--jscomp_error=checkRegExp',
-            '--warning_level=VERBOSE',
-            '--jscomp_warning=checkDebuggerStatement',
+          '--jscomp_error=constantProperty',
+          '--jscomp_error=const',
+          '--jscomp_error=checkRegExp',
+          '--warning_level=VERBOSE',
+          '--jscomp_warning=checkDebuggerStatement',
 
-            '--js=app/lib/google-closure-library/closure/goog/**.js',
-            '--js=!app/lib/google-closure-library/closure/goog/**test.js',
-            '--js=app/lib/google-closure-library/third_party/closure/goog/**.js',
-            '--js=!app/lib/google-closure-library/third_party/closure/goog/**test.js',
-            '--js=app/js/KeepMeContributing/**.js',
-            '--js_output_file=app/test/js/app.js'
-          ].join(' ')
-        )
+          '--js=app/lib/google-closure-library/closure/goog/**.js',
+          '--js=!app/lib/google-closure-library/closure/goog/**test.js',
+          '--js=app/lib/google-closure-library/third_party/closure/goog/**.js',
+          '--js=!app/lib/google-closure-library/third_party/closure/goog/**test.js',
+          '--js=app/js/KeepMeContributing/**.js',
+          '--js_output_file=app/test/js/app.js'
+        ].join(' ')
       },
       testServer: { command: 'js-dev-server -S app/ -W app/ --port 9876' },
       lint: { command: 'eslint Gruntfile.js app/js/' }

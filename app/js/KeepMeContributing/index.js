02c23371fafedd8d1bee8c497fafc8499b5e8476
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2015-08-16 21:48:49 +0900

    simple test app to check the behaviour and appearance

--- app/js/KeepMeContributing/index.js
+++ app/js/KeepMeContributing/index.js
@@ -1,5 +1,24 @@
-/*global goog:false*/
+/*global goog:false, KeepMeContributing:false*/
 
 goog.provide('KeepMeContributing');
+
 goog.require('KeepMeContributing.ContributionStatus');
 goog.require('KeepMeContributing.Github');
+
+goog.require('KeepMeContributing.ContributionStatusView');
+
+goog.require('goog.dom');
+
+KeepMeContributing.main = function(){
+  let kmc = KeepMeContributing;
+  let /** string */ username = 'igrep';
+  let /** ContributionStatus */ status = new kmc.ContributionStatus(
+    new kmc.Github({ username: username, apiUrl: '/test/fixtures/github.com' })
+  );
+  let /** ContributionStatusView */ view = new kmc.ContributionStatusView(
+    username, status
+  );
+
+  view.render(goog.dom.getElement('contributionStatus'));
+  status.startPolling(1000);
+};

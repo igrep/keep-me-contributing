8301f6bb448978024959f0e76b201acb87b5e2f8
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2015-08-22 09:43:18 +0900

    write GithubTest instead of AppTest

--- src/test/java/info/igreque/keepmecontributing/github/GithubTest.java
+++ src/test/java/info/igreque/keepmecontributing/github/GithubTest.java
@@ -1,4 +1,4 @@
-package info.igreque.keepmecontributing;
+package info.igreque.keepmecontributing.github;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -7,7 +7,7 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest
+public class GithubTest
     extends TestCase
 {
     /**
@@ -15,7 +15,7 @@ public class AppTest
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public GithubTest(String testName)
     {
         super( testName );
     }
@@ -25,7 +25,7 @@ public class AppTest
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( GithubTest.class );
     }
 
     /**

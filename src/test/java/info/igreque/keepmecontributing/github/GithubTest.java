c940479335b4e4e5bc492665a00410d1824e20e4
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2015-08-22 14:13:40 +0900

    prepare the 1st test

--- src/test/java/info/igreque/keepmecontributing/github/GithubTest.java
+++ src/test/java/info/igreque/keepmecontributing/github/GithubTest.java
@@ -1,14 +1,44 @@
 package info.igreque.keepmecontributing.github;
 
-import static org.junit.Assert.assertEquals;
+import java.util.HashMap;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 import org.junit.Test;
+import org.junit.Before;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+import info.igreque.keepmecontributing.github.Github;
+import info.igreque.keepmecontributing.github.Contributions;
 
 /**
  * Unit test for Github API client class
  */
 public class GithubTest {
+  private Github describedInstance;
+
+  @Before
+  public void setUp(){
+    describedInstance = new Github("igrep", null);
+  }
+
   @Test
-  public void runsTests(){
-    assertEquals("The truth", 1, 2);
+  public void fetchesContributionsSvg(){
+    assertEquals("The truth", 1, 1);
+  }
+
+  @Test
+  public void convertsContributionsSvgToMap() throws FileNotFoundException {
+    HashMap<String, Contributions> result = describedInstance.contributionsCalendarFromSvg(
+      new FileInputStream(
+        Github.class.getClassLoader().getResource("contributions-2015-08-22.svg").getPath()
+      )
+    );
+    assertThat("returns a map containing the latest contributions count",
+      result.get("2015-08-22").length, is(1000)
+    );
   }
 }

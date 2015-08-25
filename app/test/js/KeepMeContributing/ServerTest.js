e94e5401005d92151b01462e4caa2888ac01b2d4
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2015-08-25 22:45:08 +0900

    fix test error
    I'm not sure why, but the xhr gets cleaned up when goog.Promise#resolve-ed
    So xhr.getResponseJson() returned undefined before this change

--- app/test/js/KeepMeContributing/ServerTest.js
+++ app/test/js/KeepMeContributing/ServerTest.js
@@ -7,28 +7,21 @@ describe('The API server', function(){
   this.timeout(5000);
 
   describe('/users/:username/contributions.json', function(){
-    before(function(){
-      this.subjectFunction = function(){
-        return new goog.Promise(function(resolve){
-          goog.net.XhrIo.send(
-            '/users/igrep/contributions.json',
-            function(event){ resolve(event.target); },
-            'GET'
-          );
-        });
-      };
-    });
-
     it('returns a JSON with 200 OK', function(done){
-      this.subjectFunction().then(function(xhr){
-        let responseJson = xhr.getResponseJson();
-        expect(responseJson).to.be.an('object');
-        expect(responseJson).not.to.be.empty();
+      goog.net.XhrIo.send(
+        '/users/igrep/contributions.json',
+        function(event){
+          let xhr = event.target;
+          let responseJson = xhr.getResponseJson();
+          expect(responseJson).to.be.an('object');
+          expect(responseJson).not.to.be.empty();
 
-        expect(xhr.getStatus()).to.be(200);
+          expect(xhr.getStatus()).to.be(200);
 
-        done();
-      });
+          done();
+        },
+        'GET'
+      );
     });
   });
 });
